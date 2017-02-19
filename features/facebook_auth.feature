0b8a8f4296ee9d860a56790519c0f3a311eb3b24
h2c <arthurcyu89@gmail.com>
2017-02-18 23:08:33 -0800

    Complete all 4 steps

--- features/facebook_auth.feature
+++ features/facebook_auth.feature
@@ -1,11 +1,11 @@
 Feature: getting app token
 
   Scenario: As facebook developer, I need to generate app token
-    Given a "GET" request is made to "/oauth/access_token"
+    Given I make a "GET" request to "/oauth/access_token"
     When these parameters are supplied in URL
       |query         | param                            |
       |grant_type    | client_credentials               |
       |client_id     | 931957476934241                  |
       |client_secret | 68b3a63cfb0a6e961978ddd205e005f2 |
-#    Then the api call should succeed
-#    And value of access token is saved in a global variable
+    Then the api call should "succeed"
+    And value of access token is saved in a global variable
--- features/steps/common_steps.py
+++ features/steps/common_steps.py
@@ -1,40 +1,69 @@
-from behave import *
+from behave import given, when, then, step
 import urllib
+import requests
 
 use_step_matcher("parse")
 hostname = "https://graph.facebook.com"
 
-@given('a "{method}" request is made to "{path}"')
+@given('I make a "{method}" request to "{path}"')
 def step_make_request(context, method, path):
     context.uri = hostname + path
-    context.callMethod = method
+    context.method = method
     pass
 
 
 @when(u'these parameters are supplied in URL')
 def step_verify_parameters(context):
-    print (context.uri)
+
     context.uri += "?"
     for row in context.table:
         context.uri = context.uri + urllib.urlencode({ row[0]: row[1] })
         context.uri += "&"
     context.uri = context.uri[:-1]
-    print (context.uri)
     pass
 
 
-@step("the api call should succeed")
-def step_verify_api_call_succeed(step):
-    """
-    :type step: lettuce.core.Step
-    """
+@then('the api call should "{condition}"')
+def step_verify_api_calls(context, condition):
+    # Determine method and make HTTP Request
+    # No python native switch statement, workaround is more tedious than chained if-else
+    # Couldnt find binary input for steps in matcher "parse" for "{condition}"
+
+    if context.method == "GET":
+        print ("Request method: GET")
+        print ("Request URI: ", context.uri)
+        try:
+            response = requests.get(context.uri)
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as err:
+            context.error = err
+
+        print ("Response status: ", response.status_code)
+        print ("Response body: ", response.content)
+    elif context.method == "POST":
+        print ("Request method: POST")
+    elif context.method == "PUT":
+        print ("Request method: PUT")
+    elif context.method == "DELETE":
+        print ("Request method: DELETE")
+    else:
+        raise ("Please use correct verb!!!")
+
+    # Determine if succeed or fail
+    if condition == 'succeed':
+        assert hasattr(context, 'error') is False
+        # Store access token
+        context.responseContent = response.content
+    elif condition == 'fail':
+        assert hasattr(context, 'error') is True
+    else:
+        raise ("Please use succeed/fail as condition!")
+    print("")
     pass
-
-
 
 @step("value of access token is saved in a global variable")
-def step_verify_access_token_stored_in_var(step):
-    """
-    :type step: lettuce.core.Step
-    """
+def step_verify_access_token_stored_in_context(context):
+    accessToken =  context.responseContent[len("access_token="):]
+    print ("Access Token: ", accessToken)
+    context.accessToken = accessToken
     pass
